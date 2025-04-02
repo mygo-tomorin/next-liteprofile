c684049d09b1189fcfb9e2e086dbddbee55a24d9
mygo-tomorin <110090@mail2.chshs.ntpc.edu.tw>
2025-04-02 11:57:29 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import Projects from "@/components/Projects";
+
 import TabInterface from "@/components/TabInterface";
-import Contest from "@/components/Contest";
-import SocialLink from "@/components/SocialLink";
+
 import Radar from "@/components/Radar";
 
 export default function Home() {
@@ -14,9 +13,7 @@ export default function Home() {
         <AboutSection />
         <Radar />
         <TabInterface />
-        <Contest />
-        <Projects />
-        <SocialLink />
+    
       </div>
     </main>
   );
